c30af282e406d1198b2d23b461818c53532df552
yiminl18 <32020422+yiminl18@users.noreply.github.com>
2020-11-18 11:38:09 -0800

    update static devices filtering

--- src/main/java/edu/uci/ics/deid/service/occupancy/OccupancyAnalysis.java
+++ src/main/java/edu/uci/ics/deid/service/occupancy/OccupancyAnalysis.java
@@ -39,6 +39,9 @@ public class OccupancyAnalysis implements DisposableBean, Runnable {
     @Value("${occupancy.parameter.interval}")
     private double interval;
 
+    @Value("${occupancy.parameter.staticDeviceThreshold}")
+    private long staticDeviceThreshold;
+
     @Value("${occupancy.input_data.WiFiAPs}")
     private String wifiAPFile;
 
@@ -54,6 +57,9 @@ public class OccupancyAnalysis implements DisposableBean, Runnable {
     @Value("${occupancy.input_data.cluterLabel}")
     private String clusterLabelFile;
 
+    @Value("${occupancy.parameter.updateInterval}")
+    private long updateInterval;
+
     @Autowired
     RawEventRecvQueue recvQueue;
 
@@ -64,6 +70,7 @@ public class OccupancyAnalysis implements DisposableBean, Runnable {
     Map<String, Integer> staticDevice = new HashMap<String, Integer>();
     List<String> aps = new ArrayList<>();
 
+    private Timestamp lastWeekTimestamp;//use to update staticDevices weekly
     private Timestamp lastTimestamp;
     private Timestamp currentTimestamp;
     private long timeElapsed;
@@ -82,6 +89,7 @@ public class OccupancyAnalysis implements DisposableBean, Runnable {
     public void run(){
         RawConnectionEvent evt = null;
         //lastTimestamp = new Timestamp(System.currentTimeMillis());//modify to first event for testing
+        lastWeekTimestamp = new Timestamp(System.currentTimeMillis());
         SO.readGraph(graphFile, clusterLabelFile);
         mapAP();
 
@@ -89,17 +97,38 @@ public class OccupancyAnalysis implements DisposableBean, Runnable {
             //read event from queue
             try {
                 evt = recvQueue.take();
-                //filterStaticDevice(evt);
+                filterStaticDevice(evt);
                 assignEvent(evt);
+                updateStaticDeviceWeekly(evt);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
 
-    /*public void filterStaticDevice(RawConnectionEvent evt){
-        
-    }*/
+    public void filterStaticDevice(RawConnectionEvent evt){
+        int hour = evt.getTimestamp().toLocalDateTime().getHour();
+        String clientMac = evt.getClientMac().getInitHashId();
+        if(hour >= staticStart && hour <= staticEnd){
+            if(!staticDevice.containsKey(clientMac)){
+                staticDevice.put(clientMac, 0);
+            }
+            else{
+                staticDevice.put(clientMac, staticDevice.get(clientMac)+1);
+            }
+        }
+    }
+
+    public void updateStaticDeviceWeekly(RawConnectionEvent evt){
+        if((evt.getTimestamp().getTime() - lastTimestamp.getTime()) >= updateInterval*24*60*60*1000){
+            for(Map.Entry<String, Integer> entry : staticDevice.entrySet()){
+                if(entry.getValue() < staticDeviceThreshold){
+                    staticDevice.remove(entry.getKey());
+                }
+            }
+            lastTimestamp = evt.getTimestamp();
+        }
+    }
 
     public String SHA1Mac(String input)
     {
@@ -145,15 +174,18 @@ public class OccupancyAnalysis implements DisposableBean, Runnable {
 
         System.out.println("event info: " + evt.getTimestamp() + " " + evt.getApId() + " " + evt.getClientMac().getMacAddrStr());
         String ap = evt.getApId();
-        if(aps.contains(ap)){//is target ap
-            String clientMac = SHA1Mac(evt.getClientMac().getMacAddrStr());
-            if(!apEvents.containsKey(ap)){
-                List<String> macs = new ArrayList<>();
-                macs.add(clientMac);
-                apEvents.put(ap, macs);
-            }
-            else{
-                apEvents.get(ap).add(clientMac);
+        String clientMac = evt.getClientMac().getInitHashId();
+
+        if(!staticDevice.containsKey(clientMac)){//not a static device
+            if(aps.contains(ap)){//is target ap
+                if(!apEvents.containsKey(ap)){
+                    List<String> macs = new ArrayList<>();
+                    macs.add(clientMac);
+                    apEvents.put(ap, macs);
+                }
+                else{
+                    apEvents.get(ap).add(clientMac);
+                }
             }
         }
 
